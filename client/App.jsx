af9362b59ea5038edfd4d49864d369e994abc4e7
Alexandra Sandoval <alexandoval13@gmail.com>
2020-12-28 18:19:31 -0800

    Write App container, export to render

---
+++ client/App.jsx
@@ -0,0 +1,21 @@
+import React, { useState, useEffect, useRef } from 'react';
+import ReactDOM from 'react-dom';
+
+import axios from 'axios';
+
+
+
+const App = () => {
+
+  useEffect( () => {
+
+  }, [])
+
+  return (
+    <div >
+      APP CONTAINER
+    </div>
+  )
+}
+
+export default App;
