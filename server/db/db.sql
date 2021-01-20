74ef48e3365d9959ba595f5e4308396f6b29aea0
Alexandra Sandoval <alexandoval13@gmail.com>
2021-01-19 19:19:50 -0800

    Fix styntax errors in PG schema

--- server/db/db.sql
+++ server/db/db.sql
@@ -3,24 +3,32 @@ DROP DATABASE IF EXISTS planthero;
 
 CREATE DATABASE planthero;
 
- \c planthero;
+\c planthero;
 
- CREATE TABLE IF NOT EXISTS user (
-   id serial primary key,
-   user_name text not null,
-   user_pw text not null
- );
+CREATE TABLE IF NOT EXISTS users (
+  id SERIAL PRIMARY KEY,
+  user_name VARCHAR(30) NOT NULL,
+  user_pw VARCHAR(30) NOT NULL
+);
 
- CREATE TABLE IF NOT EXISTS plants (
-   id serial primary key,
-   plant_name VARCHAR(30) not null,
-   nickname VARCHAR(30),
-   light VARCHAR(30),
-   watering_times int not null,
-   watering_weeks int not null,
-   last_watered date not null,
-   humidity VARCHAR(10),
-   photoURL VARCHAR,
- );
+CREATE TABLE IF NOT EXISTS userplants (
+  id SERIAL PRIMARY KEY,
+  plant_name VARCHAR(30) NOT NULL,
+  nickname VARCHAR(30),
+  light VARCHAR(30),
+  watering_times INT NOT NULL,
+  watering_weeks INT NOT NULL,
+  last_watered DATE NOT NULL,
+  humidity VARCHAR(10),
+  photoURL VARCHAR,
+  user_id INT NOT NULL REFERENCES users(id)
+);
+
+CREATE TABLE IF NOT EXISTS usercategories (
+  id SERIAL PRIMARY KEY,
+  category_value VARCHAR(30),
+  category_label VARCHAR(30),
+  user_id INT NOT NULL REFERENCES users(id)
+);
 
  -- NOTE can calculate "age" with AGE() function
